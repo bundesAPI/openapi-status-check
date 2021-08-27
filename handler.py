16f356cac7c6cb29e811f7458343011289cdd28a
Lilith Wittmann <mail@lilithwittmann.de>
2021-08-27 13:30:09 +0200

    secrets

--- handler.py
+++ handler.py
@@ -6,8 +6,7 @@ import requests
 
 class StatuspageClient:
     def __init__(self):
-        self.page_id = ""
-        self.api_key = ""
+        self.page_id, self.api_key = get_secret()
 
         self.headers = {
             'Authorization': self.api_key,
@@ -114,8 +113,60 @@ class SchemaChecker:
 
         return paths
 
+
+import boto3
+import base64
+from botocore.exceptions import ClientError
+
+
+def get_secret():
+
+    secret_name = "statuspage"
+    region_name = "eu-central-1"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    # In this sample we only handle the specific exceptions for the 'GetSecretValue' API.
+    # See https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+    # We rethrow the exception by default.
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'DecryptionFailureException':
+            # Secrets Manager can't decrypt the protected secret text using the provided KMS key.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'InternalServiceErrorException':
+            # An error occurred on the server side.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'InvalidParameterException':
+            # You provided an invalid value for a parameter.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'InvalidRequestException':
+            # You provided a parameter value that is not valid for the current state of the resource.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'ResourceNotFoundException':
+            # We can't find the resource that you asked for.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+    else:
+       return get_secret_value_response['STATUSPAGE_PAGE_ID'], get_secret_value_response['STATUSPAGE_PAGE_SECRET']
+
+
 #help(schema)
 def run(event, context):
+
     name = "autobahn-api"
     description = """Was passiert auf Deutschlands Bundesstraßen? API für aktuelle Verwaltungsdaten zu Baustellen, Staus und Ladestationen. Außerdem Zugang zu Verkehrsüberwachungskameras und vielen weiteren Datensätzen.
     """
